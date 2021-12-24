415c77cf8dee2903bdd83f25fc1a772037aecf5e
Albin N. Karmo <albinkarmo@gmail.com>
2021-12-24 17:45:42 +0000

    Configuration to deploy to heroku

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ let express = require ('express');
 let http = require('http');
 let app = express();
 let path = require("path");
-let PORT = 3000;
-
+let PORT = process.env.PORT || 3000;
+const axios = require("axios");
+// api url
+// const api_url = "https://kpalan-event.herokuapp.com/";
+  
+// Defining async function
+// async function getapi() {
+//     const res = await axios("https://kpalan-event.herokuapp.com/participant");
+//     const data = res.data;
+//     console.log((JSON.stringify({data})));
+    
+// // axios.get('http://example.com/movies.json')
+// //   .then(response => response.json())
+// //   .then(data => console.log(data));
+// }
+// // Calling that async function
+// getapi();
 
 app.use(express.static(path.join(__dirname, 'public')))
 let file = "./public/index.html"
